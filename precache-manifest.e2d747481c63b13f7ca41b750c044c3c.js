fd6af27a48cea99d1d2e7644050e6e30d70a0346
Tish <latisha.griffiths@programmer.net>
2020-01-05 05:12:14 -0600

    Updates

---
+++ precache-manifest.e2d747481c63b13f7ca41b750c044c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17f420658d92f40ea6ddbb96a8e906c",
+    "url": "/tg-calculator/index.html"
+  },
+  {
+    "revision": "9502edbeef1cd512e3b3",
+    "url": "/tg-calculator/static/css/main.20167563.chunk.css"
+  },
+  {
+    "revision": "f77b7852167749928a9d",
+    "url": "/tg-calculator/static/js/2.ea88ac41.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/tg-calculator/static/js/2.ea88ac41.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9502edbeef1cd512e3b3",
+    "url": "/tg-calculator/static/js/main.a019c08c.chunk.js"
+  },
+  {
+    "revision": "2b8a76a003c4e21d503c",
+    "url": "/tg-calculator/static/js/runtime-main.9cdfb078.js"
+  }
+]);
