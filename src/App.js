3819fbd218675b2d39f25c1eeb77e58ca7b0af93
Tish <latisha.griffiths@programmer.net>
2020-01-19 22:01:27 -0600

    add transition effect when changing background image, add a default background color between changing background images

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import Calculator from "./components/Calculator";
 import { connect } from "react-redux";
 
 const App = props => {
-  const { bgImage} = props;
-  const AppStyles = {
-    backgroundImage: `url(${bgImage})`
-  };
+  const { bgImage } = props;
+  const AppStyles = bgImage
+    ? { backgroundImage: `url(${bgImage})`, transition: "0.6s background-image ease-in-out", transitionTimingFunction: 'linear', transitionDelay:"0.1s" }
+    : { backgroundColor: "rgba(0,0,0,0.6)" };
 
   return (
     <div className="app" style={AppStyles}>
       <div className="overlay">
-      <Settings />
-      <h1 className="title">TishG Calculator</h1>
-      <Calculator />
+        <Settings />
+        <h1 className="title">TishG Calculator</h1>
+        <Calculator />
       </div>
     </div>
   );
